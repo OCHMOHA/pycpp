874bff0eabb060f8facb56abced462f58f135b96
OCHMOHA <1132245115@pfur.ru>
2025-05-12 11:31:19 -0700

    Update zd_05.cpp

--- zd_05.cpp
+++ zd_05.cpp
@@ -1,49 +1,64 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
-const double g = 9.81; 
+const double g = 9.81;
 
-double calculateTime(double height) {
-    return sqrt(2 * height / g);
+double y(double T, double U, double H) {
+    return H - (g * T * T) / 2.0;
 }
 
-double calculateDistance(double velocity, double time) {
-    return velocity * time;
+double dy(double T) {
+    return -g * T;
 }
 
-double secantMethod(double (*function)(double), double x0, double x1, double epsilon = 1e-6, int maxIterations = 100) {
-    double x2;
-    for (int i = 0; i < maxIterations; ++i) {
-        if (function(x1) - function(x0) == 0) {
-            cout << "Secant method failed: f(x1) - f(x0) = 0." << endl;
+
+double newton_method(double U, double H, double T_initial, double epsilon = 1e-6, int max_iter = 100) {
+    double T = T_initial;
+    for (int i = 0; i < max_iter; ++i) {
+        double y_val = y(T, U, H);
+        double dy_val = dy(T);
+        
+        if (dy_val == 0) {
+            cout << "Derivative is zero." << endl;
             return -1;
         }
-        x2 = x1 - function(x1) * (x1 - x0) / (function(x1) - function(x0));
-        if (abs(x2 - x1) < epsilon) {
-            return x2;
+        
+        double T_new = T - y_val / dy_val;
+        
+        if (abs(T_new - T) < epsilon) {
+            return T_new;
         }
-        x0 = x1;
-        x1 = x2;
+        
+        T = T_new;
     }
-    cout << "Secant method did not converge after " << maxIterations << " iterations." << endl;
+    
+    cout << "Did not converge in " << max_iter << " iterations" << endl;
     return -1;
 }
 
 int main() {
-    double altitude, velocity;
-
-    cout << "Enter aircraft altitude H (in meters): ";
-    cin >> altitude;
-    cout << "Enter aircraft speed U (in m/s): ";
-    cin >> velocity;
-
-    double time = calculateTime(altitude);
-    double distance = calculateDistance(velocity, time);
-
-    cout << "Bomb flight time T: " << time << " seconds" << endl;
-    cout << "Horizontal distance S: " << distance << " meters" << endl;
-
+    double H = 50;    
+    double U = 400;   
+    
+    double T_initial = sqrt(2 * H / g);
+    double epsilon = 1e-6;
+    
+    double T = newton_method(U, H, T_initial, epsilon);
+    
+    if (T != -1) {
+        cout << fixed << setprecision(6);
+        cout << "Bomb flight time T: " << T << " seconds" << endl;
+        
+        double t;
+        cout << "Enter time t (in seconds): ";
+        cin >> t;
+        
+        double x = U * t;
+        cout << "Horizontal distance x(t): " << x << " meters" << endl;
+    }
+    
     return 0;
 }
